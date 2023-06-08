eb2211e5ff4ecf47da6b773a141a4290d011257f
alee <a.fikri2411@gmail.com>
2023-06-08 14:09:07 +0700

    first commit

---
+++ src/notes.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+//  notes = [
+//   {
+//     id: string,
+//     title: string,
+//     createdAt: string,
+//     updatedAt: string,
+//     tags: [string],
+//     body: string,
+//   },
+// ];
+
+const notes = [];
+module.exports = notes;
